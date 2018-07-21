f9104eb27a3e82bd1e910914ae557a90861af789
Fabricio <carolinoreinaldo@gmail.com>
2018-07-20 23:14:22 -0300

    instalação e configuração do ejs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
+//configura o ejs
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('home'))
 
 app.listen(process.env.PORT || 5000)
